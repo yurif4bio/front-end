b70ef30baf4560249eed13a56da9d0baf4c21fc9
Yuri <yuri.fabio@escolar.ifrn.edu.br>
2025-10-22 17:34:53 -0300

    primeiro exemplo

--- estudo-01/src/app/app.ts
+++ estudo-01/src/app/app.ts
@@ -8,5 +8,18 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+  numero = 0
+
+  incrementar(){
+    if (this.numero < 10) {
+      this.numero++;
+    }
+  }
+
+  decrementar(){
+    if (this.numero > 0) {
+      this.numero--;
+    }
+  }
   protected readonly title = signal('estudo-01');
 }
